fb1f891f6f607cfcb6d13782444252333490c092
Miftahul Jannat <132377331+Miftahul220@users.noreply.github.com>
2025-08-06 00:18:57 +0600

    Tutorial 7

---
+++ Cls7-1.js
@@ -0,0 +1,3 @@
+console.log("Miftahul jannat");
+   console.log(2000);/*my income */
+    console.log(1500);/*my expenses*/
---
+++ Cls7-2.js
@@ -0,0 +1,8 @@
+const userName ='mifta';
+    const income ='100';
+    const expenses ='90';
+    console.log ('parsonal Budget Tracked app ') ;
+    
+    console. log (userName) ;
+    console. log (income) ; /*my income */
+    console. log (expenses) ; /*my expenses*/
